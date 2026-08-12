14ad25cdc6b14111b5ea811dd84013654f86ed76
pranav5601 <35168673+pranav5601@users.noreply.github.com>
2026-08-12 13:13:15 +0530

    Cors added

--- index.js
+++ index.js
@@ -6,8 +6,21 @@ const userRouter = require("./router/user_router.js");
 const ServerlessHttp = require("serverless-http");
 const app = express();
 const port = 3030;
+const cors = require("cors");
 
 // Middleware to parse JSON bodies
+
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173", // Allow your local Vite dev server    // Allow your deployed frontend later
+        ],
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+    })
+);
+
 app.use(express.json());
 app.use(noteRouter);
 app.use(reminderRouter);
